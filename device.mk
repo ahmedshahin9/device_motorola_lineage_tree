1dbc2b218d9a0b8120766de84666212cf66e850d
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-05-15 21:20:53 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/motorola/penangf
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
