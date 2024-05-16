3ee60984e16860804d1ca5a7da04f36662da9d5a
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-05-16 18:06:37 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_penangf.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage-penangf-user \
-    lineage-penangf-userdebug \
-    lineage-penangf-eng
+    lineage_penangf-user \
+    lineage_penangf-userdebug \
+    lineage_penangf-eng
