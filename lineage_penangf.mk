7b0fdee691b64bbf348725389830f73dae74c101
Ahmed Shahin <83367407+ahmedshahin9@users.noreply.github.com>
2024-05-16 06:49:00 +0300

    Update lineage_penangf.mk

--- lineage_penangf.mk
+++ lineage_penangf.mk
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-DEVICE_PATH := device/motorola/penangf
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
